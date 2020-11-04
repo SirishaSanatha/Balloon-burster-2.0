2f71ad968299c350421f9d3e4c64e3e344cd4e3e
Sirisha Sanatha <41163436+SirishaSanatha@users.noreply.github.com>
2020-11-04 14:07:52 +0530

    updating music

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function preload(){
   blue_balloonImage = loadImage("blue_balloon0.png");
   color_balloonImage=loadImage("colorb.png");
   cactusImage=loadImage("cactus.png");
-  music=loadSound("music.mp3");
+  music=loadSound("music.wav");
   heartImage=loadImage("heart.png");
 }
 
